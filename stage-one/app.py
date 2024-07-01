799cbff4db283784ca8c39d5db684f4f1d58a581
adannalyn <qvexvon@gmail.com>
2024-07-01 05:41:36 +0100

    jsonify

--- stage-one/app.py
+++ stage-one/app.py
@@ -4,17 +4,19 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    visitor_name = request.args.get('visitor_name', 'Guest')
+    visitor_name = request.args.get('visitor_name')
     client_ip = request.remote_addr
     location = "New Yok"
     temperature = 11
     greeting = f"Hello, {visitor_name}! The temperature is {temperature} degrees celsius in {location}"
     
-    return jsonify({
+    response_data = {
         "client_ip": client_ip,
         "location": location,
         "greeting": greeting
-    })
+    }
+    
+    return jsonify(response_data)
     
 if __name__ = '__main__':
     app.run(debug=True)
