3b181df0a3161aad8ade6a715641a6371aa3d345
adannalyn <qvexvon@gmail.com>
2024-07-01 19:04:57 -0700

    web json

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name', 'Visitor')
 
-    client_ip = request.remote_addr
+    client_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
 
     location_url = f'http://ip-api.com/json/{client_ip}'
     location_response = requests.get(location_url).json()
     city = location_response.get('city', 'Unknown')
 
     weather_api_key = '5e3096a01a2e3f37899007dfa3ee3fca'
-    weather_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={weather_api_key}&units=metric'
+    weather_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
     weather_response = requests.get(weather_url).json()
     temperature = weather_response['main']['temp']
 
-    greeting = f"Hello, {visitor_name}! the temperature is {temperature} degrees celsius in {city}"
+    greeting = f'Hello, {visitor_name}! the temperature is {temperature} + degrees celsius in {city}'
 
     response_data = {
-        "client_ip": client_ip,
-        "location": city,
-        "greeting": greeting
+        'client_ip': client_ip,
+        'location': city,
+        'greeting': greeting
     }
 
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=9157)
