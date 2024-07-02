2f2969611208b71f3976816732dd2d7656e795b8
adannalyn <qvexvon@gmail.com>
2024-07-02 09:05:53 +0100

    jsonify

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def hello():
     visitor_name = request.args.get('visitor_name', 'Visitor')
 
-    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    client_ip = request.remote_addr
 
     location_url = f'http://ip-api.com/json/{client_ip}'
-    location_response = requests.get(location_url).json()
+    location_response = requests.get{location_url}.json()
     city = location_response.get('city', 'Unknown')
 
     api_key = '5e3096a01a2e3f37899007dfa3ee3fca'
@@ -19,7 +19,7 @@ def hello():
     weather_response = requests.get(weather_url).json()
     temperature = weather_response['main']['temp']
 
-    greeting = f'Hello, {visitor_name}! the temperature is {temperature} degrees celsius in {city}'
+    greeting = f'Hello, {visitor_name}! the temperature is {temperature} degrees Celsius in {city}'
 
     response_data = {
         'client_ip': client_ip,
@@ -28,7 +28,3 @@ def hello():
     }
 
     return jsonify(response_data)
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
